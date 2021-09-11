ad6d63d9c86aca90834b3c1b89653e753cd193af
HyperGamesDev <hypergamesdev@gmail.com>
2021-09-11 16:03:54 +0200

    Game done?

--- HaipaDebut/Assets/Scripts/PCManager.cs
+++ HaipaDebut/Assets/Scripts/PCManager.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PCManager : MonoBehaviour{
     [SerializeField] GameObject[] panels;
+    [SerializeField] GameObject buttons;
     [SerializeField] int currentPanel;
     IEnumerator Start(){
         AudioManager.instance.Play("keyboardTyping");
         TurnOffAllPanels();
+        buttons.SetActive(false);
         yield return new WaitForSeconds(3.55f);
         TurnOnPanel(0);
+        buttons.SetActive(true);
     }
     
     void TurnOffAllPanels(){foreach(GameObject go in panels){go.SetActive(false);}}
     public void TurnOnPanel(int i){panels[i].SetActive(true);currentPanel=i;}
-    public void TurnOnNextPanel(){if(currentPanel<panels.Length){TurnOnPanel(currentPanel+1);}}
-    public void TurnOnPrevPanel(){if(currentPanel>0){TurnOnPanel(currentPanel-1);}}
+    public void TurnOnNextPanel(){if(currentPanel<panels.Length-1){TurnOffAllPanels();TurnOnPanel(currentPanel+1);}}
+    public void TurnOnPrevPanel(){if(currentPanel>0){TurnOffAllPanels();TurnOnPanel(currentPanel-1);}}
 }
